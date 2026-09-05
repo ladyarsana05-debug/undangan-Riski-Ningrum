77170aed430329c27cf24b18a5b09ca73181b074
ladyarsana05-debug <ladyarsana05@gmail.com>
2026-09-06 02:33:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const opening = document.getElementById("opening");
+    const mainContent = document.getElementById("mainContent");
+    const openInvitation = document.getElementById("openInvitation");
+
+
+    openInvitation.addEventListener("click", function () {
+
+        opening.style.opacity = "0";
+
+
+        setTimeout(function () {
+
+            opening.style.display = "none";
+
+            mainContent.classList.remove("hidden");
+
+            document.body.classList.remove("locked");
+
+        }, 800);
+
+    });
+
+});
